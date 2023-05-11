78c132b10217d3c657a5648f0722a8f4ffedf01b
yseo14 <lmslys14@naver.com>
2023-05-11 19:53:09 +0900

    지도API 네이버로 변경
    의존성 변경, libs, jniLibs 제거

--- app/src/main/java/com/example/spotnow/HomeFragment.java
+++ app/src/main/java/com/example/spotnow/HomeFragment.java
@@ -18,10 +18,10 @@ import androidx.fragment.app.Fragment;
 
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
-import net.daum.mf.map.api.MapView;
-
+import com.naver.maps.map.MapView;
 
 public class HomeFragment extends Fragment {
+
 
     private MapView mapView; // MapView 객체 선언
     private Button myButton; // 버튼 객체 선언
@@ -35,16 +35,10 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.home_fragment, container, false);
 
-        ViewGroup mapViewContainer = rootView.findViewById(R.id.map_view);
 
-        if (mapView != null) {
-            // 기존에 생성된 MapView 객체가 있다면 제거
-            mapViewContainer.removeView(mapView);
-        }
+        mapView = rootView.findViewById(R.id.map_fragment);
+        mapView.onCreate(savedInstanceState);
 
-        // 새로운 MapView 객체 생성
-        mapView = new MapView(getContext());
-        mapViewContainer.addView(mapView);
 
         myButton = rootView.findViewById(R.id.reset_location_button);
         myButton.setOnClickListener(new View.OnClickListener() {
